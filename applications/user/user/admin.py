11745158f6ff890d4fad4011afbaa0dcf79f1320
25th-Night <brown_eyed87@naver.com>
2023-10-31 00:05:27 +0900

    Feat)admin: User 모델을 admin 사이트에 추가

--- applications/user/user/admin.py
+++ applications/user/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "email", "fullname", "phone", "is_admin"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["email", "fullname", "phone"]
