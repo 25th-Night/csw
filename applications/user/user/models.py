6311336376e9a25ce42b83c15749a08150f6ace3
25th-Night <brown_eyed87@naver.com>
2023-10-31 00:01:44 +0900

    Feat)models: User 모델 생성

--- applications/user/user/models.py
+++ applications/user/user/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+from common.models import CommonModel
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **kwargs):
@@ -25,3 +27,31 @@ class UserManager(BaseUserManager):
         superuser.is_superuser = True
         superuser.save(using=self._db)
         return superuser
+
+
+class User(CommonModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="이메일", max_length=100, unique=True)
+    fullname = models.TextField(verbose_name="이름", max_length=30)
+    phone = models.TextField(verbose_name="휴대폰번호", max_length=30, unique=True)
+    password = models.CharField(verbose_name="비밀번호", max_length=100)
+    is_admin = models.BooleanField(verbose_name="관리자여부", default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["password"]
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자 목록"
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["-created_at"]),
+        ]
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
