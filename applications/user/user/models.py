e2646a4b5ae3814177aa460e5dc743d0862f8cb4
25th-Night <brown_eyed87@naver.com>
2023-10-30 23:50:52 +0900

    Feat)models: UserManager 추가

--- applications/user/user/models.py
+++ applications/user/user/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        superuser = self.create_user(email=email, password=password)
+
+        superuser.is_admin = True
+        superuser.is_superuser = True
+        superuser.save(using=self._db)
+        return superuser
