798d71f6cba8fa48f9cf8e83d6e749ee82b38d76
25th-Night <brown_eyed87@naver.com>
2023-10-31 00:01:27 +0900

    Feat)settings: AUTH_USER_MODEL 지정

--- applications/user/config/settings.py
+++ applications/user/config/settings.py
@@ -160,3 +160,6 @@ STATIC_ROOT = "/var/www/html/static"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# AUTH_USER_MODEL
+AUTH_USER_MODEL = "user.User"
