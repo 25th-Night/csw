ed5c47f003b6b4e681c3a8e4adcb0be57822727d
25th-Night <brown_eyed87@naver.com>
2023-11-02 00:13:42 +0900

    update)settings: secret key 환경변수로 변경

--- applications/shortener/config/settings.py
+++ applications/shortener/config/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ai9omaxa8#(_=q8_buzs*7y8@9k$tmrxbq^@m6%&mzy!^doi2j"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
