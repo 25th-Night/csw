075d434427d9ed617303d4183beacb44291e86d8
25th-Night <brown_eyed87@naver.com>
2023-11-02 00:13:42 +0900

    update)settings: secret key 환경변수로 변경

--- applications/user/config/settings.py
+++ applications/user/config/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%&!+cq=t3&oacn3&-pe3^r1e(d-x()gmu=_6=9y(!l@3l9yj=c"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
