9807af356916277012b360500f188168d1eb862c
25th-Night <brown_eyed87@naver.com>
2023-11-02 13:13:19 +0900

    update)subnet: vpc와 모듈 분리

--- infra/AWS/modules/subnet/main.tf
+++ infra/AWS/modules/subnet/main.tf
@@ -17,40 +17,17 @@ data "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id                  = data.aws_vpc.main.id
-  cidr_block              = cidrsubnet(data.aws_vpc.main.cidr_block, 8, 1)
-  availability_zone       = "ap-northeast-2a"
+  cidr_block              = cidrsubnet(data.aws_vpc.main.cidr_block, 8, var.subnet_netnum)
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "server-subnet"
+    Name = "server-subnet-${var.name}"
     Env  = var.env
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = data.aws_vpc.main.id
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = data.aws_vpc.main.id
-
-  tags = {
-    Name = "main-${var.env}"
   }
 }
 
 resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_route" "main" {
-  route_table_id         = aws_route_table.main.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.gw.id
-  depends_on             = [aws_route_table.main]
+  route_table_id = var.route_table_id
 }
