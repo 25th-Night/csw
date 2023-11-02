9c7ac2fbd1453662383c1deee1950c24b1d6ddab
25th-Night <brown_eyed87@naver.com>
2023-11-02 14:20:59 +0900

    feat)settings: 환경별 설정 파일 분리

---
+++ applications/shortener/config/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
+        "OPTIONS": {"options": "-c search_path=shortener,public"},
+    }
+}
