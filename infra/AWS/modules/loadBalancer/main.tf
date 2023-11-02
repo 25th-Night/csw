13aaed3b9325b2d7ab9db0d6e51dddb84eb8829d
25th-Night <brown_eyed87@naver.com>
2023-11-02 13:11:18 +0900

    feat)loadBalancer: 모듈 수정- alb로 변경

--- infra/AWS/modules/loadBalancer/main.tf
+++ infra/AWS/modules/loadBalancer/main.tf
@@ -13,12 +13,12 @@ provider "aws" {
 
 resource "aws_lb_target_group" "main" {
   name     = "${var.name}-lb-tg-${var.env}"
-  port     = 8000
+  port     = 80
   protocol = "TCP"
   vpc_id   = var.vpc_id
   health_check {
     protocol            = "TCP"
-    port                = 8000
+    port                = 80
     interval            = 30
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -28,15 +28,15 @@ resource "aws_lb_target_group" "main" {
 resource "aws_lb_target_group_attachment" "main" {
   target_group_arn = aws_lb_target_group.main.arn
   target_id        = var.instance_id
-  port             = 8000
+  port             = 80
 }
 
 resource "aws_lb" "main" {
   name               = "${var.name}-lb-${var.env}"
   internal           = false
-  load_balancer_type = "network"
+  load_balancer_type = "application"
   subnets = [
-    var.subnet_id
+    var.subnet_id1, var.subnet_id2,
   ]
 
   tags = {
@@ -45,10 +45,28 @@ resource "aws_lb" "main" {
   }
 }
 
-resource "aws_lb_listener" "main" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "TCP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.acm_arn
 
   default_action {
     type             = "forward"
--- infra/AWS/modules/loadBalancer/variables.tf
+++ infra/AWS/modules/loadBalancer/variables.tf
@@ -10,10 +10,18 @@ variable "vpc_id" {
   type = string
 }
 
-variable "subnet_id" {
+variable "subnet_id1" {
+  type = string
+}
+
+variable "subnet_id2" {
   type = string
 }
 
 variable "instance_id" {
+  type = string
+}
+
+variable "acm_arn" {
   type = string
 }
