85f870b36a90276d4e719b8133c3e6f6739300dd
25th-Night <brown_eyed87@naver.com>
2023-11-02 13:13:19 +0900

    update)subnet: vpc와 모듈 분리

--- infra/AWS/modules/subnet/variables.tf
+++ infra/AWS/modules/subnet/variables.tf
@@ -2,6 +2,22 @@ variable "env" {
   type = string
 }
 
+variable "name" {
+  type = string
+}
+
 variable "vpc_id" {
+  type = string
+}
+
+variable "route_table_id" {
+  type = string
+}
+
+variable "subnet_netnum" {
+  type = number
+}
+
+variable "availability_zone" {
   type = string
 }
