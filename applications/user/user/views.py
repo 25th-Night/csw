98bb10e410ddc687cb24d8cb5947f4e01a4c274f
25th-Night <brown_eyed87@naver.com>
2023-11-04 00:17:50 +0900

    Feat)views: email, pw 체크 api 추가

--- applications/user/user/views.py
+++ applications/user/user/views.py
@@ -1,12 +1,16 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.hashers import check_password
+
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from user.models import User
 
 from user.serializers import SignUpSerializer, UserSerializer
 
@@ -87,5 +91,40 @@ class AuthView(GenericAPIView):
         response = Response(response_data, status=status.HTTP_202_ACCEPTED)
         response.delete_cookie("access")
         response.delete_cookie("refresh")
+        if request.user.is_authenticated:
+            logout(request)
+
+        return response
+
+
+class CheckEmailView(GenericAPIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request):
+        email = request.data.get("email")
+
+        user_exist = User.objects.filter(email=email).exists()
+
+        response_data = {"user_exist": user_exist}
+
+        response = Response(response_data, status=status.HTTP_200_OK)
+
+        return response
+
+
+class CheckPasswordView(GenericAPIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = get_object_or_404(User, email=email)
+
+        password_check = check_password(password, user.password)
+
+        response_data = {"password_check": password_check}
+
+        response = Response(response_data, status=status.HTTP_200_OK)
 
         return response
