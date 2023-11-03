fcafc57443a0e872b5bb6da359a7eabb034a5969
25th-Night <brown_eyed87@naver.com>
2023-11-03 12:37:50 +0900

    feat)views: IndexView 생성

--- applications/user/rendering/views.py
+++ applications/user/rendering/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "home/index.html"
+
+    def get(self, request, *args, **kwargs):
+        kwargs["login"] = request.COOKIES.get("access")
+        return super().get(request, *args, **kwargs)
