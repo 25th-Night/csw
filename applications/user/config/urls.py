67000d1281be727fbe374649660a30e29e5c29ce
25th-Night <brown_eyed87@naver.com>
2023-11-03 12:38:19 +0900

    feat)urls: rendering앱의 url 추가

--- applications/user/config/urls.py
+++ applications/user/config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("user/", include("user.urls")),
+    path("", include("rendering.urls")),
 ]
 
 
