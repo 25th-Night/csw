d8b9ebce0b2d64f94c414e6db1681582252836a5
25th-Night <brown_eyed87@naver.com>
2023-11-04 00:20:54 +0900

    refactor) views 파일 분리

--- applications/user/rendering/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class IndexView(TemplateView):
-    template_name = "home/index.html"
-
-    def get(self, request, *args, **kwargs):
-        kwargs["login"] = request.COOKIES.get("access")
-        return super().get(request, *args, **kwargs)
