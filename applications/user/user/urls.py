2b5f54d303b0078d600cef0e6647f3277e18aef0
25th-Night <brown_eyed87@naver.com>
2023-11-04 00:18:04 +0900

    feat)urls: 생성한 뷰의 url 추가

--- applications/user/user/urls.py
+++ applications/user/user/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from user.views import AuthView, SignUpView
+from user.views import AuthView, CheckPasswordView, CheckEmailView, SignUpView
 
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path("auth/", AuthView.as_view(), name="auth"),
+    path("signup", SignUpView.as_view(), name="signup"),
+    path("auth", AuthView.as_view(), name="auth"),
+    path("check_email", CheckEmailView.as_view(), name="check_email"),
+    path("check_pw", CheckPasswordView.as_view(), name="check_pw"),
 ]
