9388e0a16dde9f3da7ffd3aebd803b7e513b6789
25th-Night <brown_eyed87@naver.com>
2023-11-04 14:49:51 +0900

    Feat)urls: 추가한 api의 url 등록

--- applications/user/user/urls.py
+++ applications/user/user/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path, include
+from django.urls import path
 
-from user.views import AuthView, CheckPasswordView, CheckEmailView, SignUpView
+from user.views import (
+    AuthView,
+    PasswordCorrectView,
+    EmailExistView,
+    PasswordFormatView,
+    PhoneCheckView,
+    SignUpView,
+)
 
 
 urlpatterns = [
     path("signup", SignUpView.as_view(), name="signup"),
     path("auth", AuthView.as_view(), name="auth"),
-    path("check_email", CheckEmailView.as_view(), name="check_email"),
-    path("check_pw", CheckPasswordView.as_view(), name="check_pw"),
+    path("exist_email", EmailExistView.as_view(), name="exist_email"),
+    path("check_phone", PhoneCheckView.as_view(), name="check_phone"),
+    path("correct_pw", PasswordCorrectView.as_view(), name="correct_pw"),
+    path("format_pw", PasswordFormatView.as_view(), name="format_pw"),
 ]
