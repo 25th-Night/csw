6c433c9cec8f71b210fa62bad973479c7da91d4f
25th-Night <brown_eyed87@naver.com>
2023-11-05 14:22:04 +0900

    feat)urls: 생성한 뷰의 url 추가

--- applications/user/rendering/urls.py
+++ applications/user/rendering/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from rendering.views.index import IndexView
 from rendering.views.user import LoginView
+from rendering.views.url import UrlView
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
+    path("url/", UrlView.as_view(), name="url"),
 ]
