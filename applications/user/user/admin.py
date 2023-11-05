11b426e81f8a22220dfc2b0fabe4af322ebc25c4
25th-Night <brown_eyed87@naver.com>
2023-11-05 14:22:18 +0900

    feat)admin: url 어드민 추가

--- applications/user/user/admin.py
+++ applications/user/user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import Url, User
 
 
 @admin.register(User)
@@ -7,3 +7,14 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["id", "email", "fullname", "phone", "is_admin"]
     list_filter = ["created_at", "updated_at"]
     search_fields = ["email", "fullname", "phone"]
+
+
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "get_license", "total_cnt"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["user", "license"]
+
+    @admin.display(description="라이선스")
+    def get_license(self, obj):
+        return obj.get_license_display()
