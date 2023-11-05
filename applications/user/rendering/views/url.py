28f3c379f4a1abf780c430d790e32785346169fa
25th-Night <brown_eyed87@naver.com>
2023-11-05 14:21:53 +0900

    update)urls: url 페이지 렌더링 시 라이선스 정보도 전달

--- applications/user/rendering/views/url.py
+++ applications/user/rendering/views/url.py
@@ -1,9 +1,12 @@
 from django.views.generic import TemplateView
+from common.utils import get_user_url_license
+from common.permissions import LoginRequired
 
 
-class UrlView(TemplateView):
+class UrlView(LoginRequired, TemplateView):
     template_name = "url/index.html"
 
     def get(self, request, *args, **kwargs):
         kwargs["login"] = request.COOKIES.get("access")
+        kwargs["url_license"] = get_user_url_license(request)
         return super().get(request, *args, **kwargs)
