42757f67612ae7ba8c6ffe3b377a44c4c3701357
25th-Night <brown_eyed87@naver.com>
2023-11-05 14:26:52 +0900

    update)models: shortened url 모델에 접근 권한 필드 추가

--- applications/shortener/url/models.py
+++ applications/shortener/url/models.py
@@ -4,14 +4,22 @@ from common.models import CommonModel
 
 
 class ShortenedUrl(CommonModel):
-    nick_name = models.CharField(max_length=30)
-    prefix = models.CharField(max_length=50)
-    creator_id = models.IntegerField()
-    target_url = models.CharField(max_length=1000)
-    click = models.BigIntegerField(default=0)
-    shortened_url = models.CharField(max_length=6)
-    last_clicked = models.DateTimeField(null=True)
-    expired_at = models.DateField(null=True)
+    class Access(models.IntegerChoices):
+        PUBLIC = 1
+        PRIVATE = 2
+        SECRET = 3
+
+    nick_name = models.CharField(verbose_name="별칭", max_length=30)
+    prefix = models.CharField(verbose_name="접두사", max_length=50)
+    creator_id = models.IntegerField(verbose_name="생성자 ID")
+    target_url = models.CharField(verbose_name="대상 URL", max_length=1000)
+    click = models.BigIntegerField(verbose_name="클릭 수", default=0)
+    shortened_url = models.CharField(verbose_name="단축 url", max_length=6)
+    last_clicked = models.DateTimeField(verbose_name="마지막 클릭 일시", null=True)
+    expired_at = models.DateField(verbose_name="만료일", null=True)
+    access = models.IntegerField(
+        verbose_name="공개범위", choices=Access.choices, default=Access.PUBLIC
+    )
 
     class Meta:
         indexes = [
@@ -27,3 +35,6 @@ class ShortenedUrl(CommonModel):
         self.click += 1
         self.save()
         return self
+
+    def __str__(self):
+        return f"{self.target_url}의 단축 url"
