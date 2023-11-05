aec8d2981f0f0125cd8c79943df02a36aef18291
25th-Night <brown_eyed87@naver.com>
2023-11-05 14:27:15 +0900

    feat)data: 라이선스별 정보를 기록할 파일 추가

---
+++ applications/shortener/common/data.py
@@ -0,0 +1,26 @@
+License = {
+    1: {
+        "grade": "FREE",
+        "max_link_cnt": 10,
+        "monthly_limit_click": 100,
+        "allow_alias": False,
+    },
+    2: {
+        "grade": "BASIC",
+        "max_link_cnt": 300000,
+        "monthly_limit_click": 100,
+        "allow_alias": True,
+    },
+    3: {
+        "grade": "PREMIUM",
+        "max_link_cnt": float("inf"),
+        "monthly_limit_click": 100,
+        "allow_alias": True,
+    },
+    4: {
+        "grade": "MASTER",
+        "max_link_cnt": float("inf"),
+        "monthly_limit_click": 100,
+        "allow_alias": True,
+    },
+}
