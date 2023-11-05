e658a2328646bf872f4be811c0720c3b2dd743ec
25th-Night <brown_eyed87@naver.com>
2023-11-05 14:17:22 +0900

    feat)url: 유저별 라이선스 정보를 담을 url 모델 생성

--- applications/user/user/models.py
+++ applications/user/user/models.py
@@ -9,7 +9,7 @@ from common.models import CommonModel
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, phone, **kwargs):
+    def create_user(self, email, password, phone, license, **kwargs):
         if not email:
             raise ValueError("Users must have an email address")
         if not password:
@@ -18,14 +18,20 @@ class UserManager(BaseUserManager):
         user = self.model(email=email, phone=phone)
         user.set_password(password)
         user.save(using=self._db)
+
+        url = Url.objects.create(user=user)
+
         return user
 
     def create_superuser(self, email, password, phone, **kwargs):
         superuser = self.create_user(email=email, password=password, phone=phone)
-
         superuser.is_admin = True
         superuser.is_superuser = True
         superuser.save(using=self._db)
+
+        url = superuser.url.license = Url.License.MASTER
+        url.save()
+
         return superuser
 
 
@@ -55,3 +61,22 @@ class User(CommonModel, AbstractBaseUser, PermissionsMixin):
     @property
     def is_staff(self):
         return self.is_admin
+
+
+class Url(CommonModel):
+    class License(models.IntegerChoices):
+        FREE = 1
+        BASIC = 2
+        PREMIUM = 3
+        MASTER = 4
+
+    user = models.OneToOneField(
+        User, verbose_name="사용자", related_name="url", on_delete=models.CASCADE
+    )
+    license = models.IntegerField(
+        verbose_name="라이선스", choices=License.choices, default=License.FREE
+    )
+    total_cnt = models.IntegerField(verbose_name="사용 중인 총 URL 수", default=0)
+
+    def __str__(self):
+        return f"{self.user}의 라이선스 : {self.get_license_display()}"
