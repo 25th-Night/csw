6c5f872cb54a68f0a1ceac84e846a7a3b0606147
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:03:52 +0900

    update)models: shortenedurl 모델 index, ordering 설정

--- applications/shortener/url/models.py
+++ applications/shortener/url/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 from common.models import CommonModel
@@ -9,20 +10,23 @@ class ShortenedUrl(CommonModel):
         PRIVATE = 2
         SECRET = 3
 
-    nick_name = models.CharField(verbose_name="별칭", max_length=30)
+    nick_name = models.CharField(
+        verbose_name="별칭", max_length=30, blank=True, default="Undefined"
+    )
     prefix = models.CharField(verbose_name="접두사", max_length=50)
     creator_id = models.IntegerField(verbose_name="생성자 ID")
     target_url = models.CharField(verbose_name="대상 URL", max_length=1000)
     click = models.BigIntegerField(verbose_name="클릭 수", default=0)
     shortened_url = models.CharField(verbose_name="단축 url", max_length=6)
     last_clicked = models.DateTimeField(verbose_name="마지막 클릭 일시", null=True)
-    expired_at = models.DateField(verbose_name="만료일", null=True)
+    expired_at = models.DateField(verbose_name="만료일", blank=True, null=True)
     access = models.IntegerField(
         verbose_name="공개범위", choices=Access.choices, default=Access.PUBLIC
     )
 
     class Meta:
         indexes = [
+            models.Index(fields=["-created_at"]),
             models.Index(
                 fields=[
                     "prefix",
@@ -30,9 +34,11 @@ class ShortenedUrl(CommonModel):
                 ]
             ),
         ]
+        ordering = ["-created_at"]
 
     def clicked(self):
         self.click += 1
+        self.last_clicked = datetime.now()
         self.save()
         return self
 
