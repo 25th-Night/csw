97c8a2b945c2c93665d92c0f53a4f5138780fc37
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:13:49 +0900

    feat)urls: 생성한 뷰의 url 추가

--- applications/user/user/urls.py
+++ applications/user/user/urls.py
@@ -2,19 +2,23 @@ from django.urls import path
 
 from user.views import (
     AuthView,
+    TokenRefreshView,
     PasswordCorrectView,
     EmailExistView,
     PasswordFormatView,
     PhoneCheckView,
     SignUpView,
+    UpdateTotalUrlCountView,
 )
 
 
 urlpatterns = [
     path("signup", SignUpView.as_view(), name="signup"),
     path("auth", AuthView.as_view(), name="auth"),
+    path("refresh", TokenRefreshView.as_view(), name="refresh"),
     path("exist_email", EmailExistView.as_view(), name="exist_email"),
     path("check_phone", PhoneCheckView.as_view(), name="check_phone"),
     path("correct_pw", PasswordCorrectView.as_view(), name="correct_pw"),
     path("format_pw", PasswordFormatView.as_view(), name="format_pw"),
+    path("url", UpdateTotalUrlCountView.as_view(), name="update_url"),
 ]
