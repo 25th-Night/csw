61225c1dcb7124ffa4e53a774a2b7b1a95409420
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:09:47 +0900

    update)permissions: LoginRequired 로직 수정

--- applications/user/common/permissions.py
+++ applications/user/common/permissions.py
@@ -4,7 +4,10 @@ from django.shortcuts import redirect
 
 class LoginRequired(AccessMixin):
     def dispatch(self, request, *args, **kwargs):
-        access = request.COOKIES.get("access")
-        if not access:
+        if (
+            not hasattr(request.user, "is_authenticated")
+            or not request.user.is_authenticated
+        ):
             return redirect("login")
+
         return super().dispatch(request, *args, **kwargs)
