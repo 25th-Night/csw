3b0ff1673062ecb56e2e14157c27dfa9d7976f0c
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:06:30 +0900

    feat)permissions: IsAuthenticated 생성

--- applications/shortener/common/permissions.py
+++ applications/shortener/common/permissions.py
@@ -3,7 +3,12 @@ from rest_framework import status
 from rest_framework.request import Request
 
 from common.exceptions import GenericAPIException
-from common.utils import get_token_from_header, get_user_id_from_token
+from common.utils import get_token_from_request, get_user_id_from_request
+
+
+class IsAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        return bool(type(request.user) == dict and request.user.get("is_authenticated"))
 
 
 class LoginRequired(BasePermission):
@@ -15,7 +20,7 @@ class LoginRequired(BasePermission):
         return type(obj).__name__
 
     def has_permission(self, request: Request, view):
-        access_token = get_token_from_header(request)
+        access_token = get_token_from_request(request)
         if not access_token:
             response = {
                 "detail": "login required",
@@ -27,6 +32,6 @@ class LoginRequired(BasePermission):
             return True
 
     def has_object_permission(self, request, view, obj):
-        user_id = get_user_id_from_token(request)
+        user_id = get_user_id_from_request(request)
         if hasattr(obj, "creator_id"):
             return obj.creator_id == user_id
