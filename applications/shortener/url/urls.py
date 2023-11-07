4dd1a88c6110178a93635dbc22476c1c170e6867
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:08:00 +0900

    feat)urls: 생성한 뷰의 url 등록

--- applications/shortener/url/urls.py
+++ applications/shortener/url/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from url.views import ShortenedUrlView, UrlRedirectView
+from url.views import ShortenedUrlDetailView, ShortenedUrlView, UrlRedirectView
 
 
 urlpatterns = [
-    path("shortener/", ShortenedUrlView.as_view(), name="shortener"),
+    path("shortener/<int:pk>", ShortenedUrlDetailView.as_view(), name="url_detail"),
+    path("shortener/", ShortenedUrlView.as_view(), name="url_list"),
     path("<str:prefix>/<str:url>", UrlRedirectView.as_view(), name="redirect"),
 ]
