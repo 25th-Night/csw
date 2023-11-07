180971dae2d07ccdcddabf06c3f43a95b2eee5e1
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:16:35 +0900

    update)header: 헤더 템플릿 관련 js 작업

--- applications/user/static/js/header.js
+++ applications/user/static/js/header.js
@@ -13,42 +13,48 @@ import {
     setFetchData,
     redirectLogin,
     imageHover,
-} from './common.js';
+} from "./common.js";
 
-document.addEventListener('DOMContentLoaded', function() {
-    const urlBtn = getElFromId('header_url_btn');
-    const jdBtn = getElFromId('header_jd_btn');
-    const loginBtn = getElFromId('header_login_btn');
-    const myInfoBtn = getElFromId('header_my_info_btn');
+document.addEventListener("DOMContentLoaded", function () {
+    const adminBtn = getElFromId("header_admin_btn");
+    const urlBtn = getElFromId("header_url_btn");
+    const jdBtn = getElFromId("header_jd_btn");
+    const loginBtn = getElFromId("header_login_btn");
+    const myInfoBtn = getElFromId("header_my_info_btn");
 
     // url, jd 버튼 마우스 오버 시 효과
-    imageHover(urlBtn, '/static/img/icon/url06.png', '/static/img/icon/url05.png')
-    imageHover(jdBtn, '/static/img/icon/job-post02.png', '/static/img/icon/job-post01.png')
+    imageHover(urlBtn, "/static/img/icon/url06.png", "/static/img/icon/url05.png");
+    imageHover(jdBtn, "/static/img/icon/job-post02.png", "/static/img/icon/job-post01.png");
+
+    if (adminBtn) {
+        // admin 버튼 마우스 오버 시 효과
+        imageHover(adminBtn, "/static/img/icon/admin03.png", "/static/img/icon/admin02.png");
+        adminBtn.addEventListener("click", () => {
+            window.open("/admin/", "_blank");
+        });
+    }
 
     if (loginBtn) {
         // login 버튼 마우스 오버 시 효과
-        imageHover(loginBtn, '/static/img/icon/login02.png', '/static/img/icon/login01.png')
+        imageHover(loginBtn, "/static/img/icon/login02.png", "/static/img/icon/login01.png");
     }
 
     if (myInfoBtn) {
-        myInfoBtn.addEventListener('click', logout)
-    
+        myInfoBtn.addEventListener("click", logout);
+
         async function logout() {
-    
-            const data = setFetchData("delete", {})
-    
-            const logout_response = await fetch('/api/user/auth', data)
-    
-            if(logout_response.status == 202) {
-                location.href = '/'
+            const data = setFetchData("delete", {});
+
+            const logout_response = await fetch("/api/user/auth", data);
+
+            if (logout_response.status == 202) {
+                location.href = "/";
             } else if (logout_response.status == 400) {
                 console.log(logout_response);
             }
         }
 
         // logout 버튼 마우스 오버 시 효과
-        imageHover(myInfoBtn, '/static/img/icon/user02.png', '/static/img/icon/user01.png')
+        imageHover(myInfoBtn, "/static/img/icon/user02.png", "/static/img/icon/user01.png");
     }
 });
-
-
