aabcdbc8a40ebbb5cd3936f1f20cc3496b8e2c4c
25th-Night <brown_eyed87@naver.com>
2023-11-07 21:09:27 +0900

    feat)authentication: JWTAuthentication 추가

---
+++ applications/user/config/authentication.py
@@ -0,0 +1,35 @@
+from jwt.exceptions import DecodeError, ExpiredSignatureError
+
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authentication import BaseAuthentication
+
+from common.utils import (
+    get_token_from_request,
+    get_user_from_request,
+)
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        try:
+            access_token = get_token_from_request(request)
+            if access_token is None:
+                return (None, None)
+            user = get_user_from_request(request)
+            return (user, None)
+        except DecodeError:
+            raise AuthenticationFailed(
+                {
+                    "success": False,
+                    "detail": "잘못된 토큰입니다.",
+                    "code": "JWT_403_INVALID_ACCESSTOKEN",
+                }
+            )
+        except ExpiredSignatureError:
+            raise AuthenticationFailed(
+                {
+                    "success": False,
+                    "detail": "토큰이 만료되었습니다.",
+                    "code": "JWT_403_EXPIRED_ACCESSTOKEN",
+                }
+            )
