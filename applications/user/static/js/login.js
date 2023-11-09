eaa03d5f8541c1938e886cf82730ea90860b1cf3
25th-Night <brown_eyed87@naver.com>
2023-11-09 13:40:52 +0900

    style) 출력 메세지 영어로 변경

--- applications/user/static/js/login.js
+++ applications/user/static/js/login.js
@@ -17,36 +17,35 @@ import {
     displayErrorMessage,
     displayPermanentErrorMessage,
     setKeyForFunction,
-} from './common.js';
+} from "./common.js";
 
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener("DOMContentLoaded", function () {
     // Email Check
-    const loginEmailInput = getElFromId('login_email');
+    const loginEmailInput = getElFromId("login_email");
 
-    loginEmailInput.addEventListener('input', debounce(existUser, 500));
+    loginEmailInput.addEventListener("input", debounce(existUser, 500));
 
     async function existUser() {
         const email = loginEmailInput.value;
 
         if (email) {
             const data = setFetchData("post", { email: email });
-            const response = await fetch('/api/user/exist_email', data);
+            const response = await fetch("/api/user/exist_email", data);
 
             if (response.status === 200) {
                 const responseData = await response.json();
                 const userExist = responseData.user_exist;
-                displayPermanentErrorMessage(!userExist, "login-email", "Please check your email.")
+                displayPermanentErrorMessage(!userExist, "login-email", "Please check your email.");
             } else {
-                console.log('API 요청 실패');
+                console.log("API request failed");
             }
         }
     }
 
     // Password Check
-    const loginPasswordInput = getElFromId('login_password');
+    const loginPasswordInput = getElFromId("login_password");
 
-    loginPasswordInput.addEventListener('input', debounce(correctPassword, 500));
+    loginPasswordInput.addEventListener("input", debounce(correctPassword, 500));
 
     async function correctPassword() {
         const email = loginEmailInput.value;
@@ -54,99 +53,99 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (email) {
             const data = setFetchData("post", { email: email, password: password });
-            const response = await fetch('/api/user/correct_pw', data);
+            const response = await fetch("/api/user/correct_pw", data);
 
             if (response.status === 200) {
                 const responseData = await response.json();
                 const passwordCheck = responseData.password_check;
-                displayPermanentErrorMessage(!passwordCheck, "login-password", "Please check your password.")
+                displayPermanentErrorMessage(passwordCheck, "login-password", passwordCheck);
             } else {
-                console.log('API 요청 실패');
+                console.log("API request failed");
             }
         }
     }
 
     // Submit Login Form
-    const loginSubmitBtn = getElFromId('login_btn_email');
+    const loginSubmitBtn = getElFromId("login_btn_email");
 
-    loginSubmitBtn.addEventListener('click', login)
-    setKeyForFunction(loginSubmitBtn, 'Enter', login)
+    loginSubmitBtn.addEventListener("click", login);
+    setKeyForFunction(loginSubmitBtn, "Enter", login);
 
     async function login() {
-        const loginEmailInput = getElFromId('login_email');
-        const loginPasswordInput = getElFromId('login_password');
+        const loginEmailInput = getElFromId("login_email");
+        const loginPasswordInput = getElFromId("login_password");
         const email = loginEmailInput.value;
         const password = loginPasswordInput.value;
 
-        
-
         if (!email) {
-            displayErrorMessage('login-email', 'Please enter your email.');
+            displayErrorMessage("login-email", "Please enter your email.");
             return;
         }
 
         if (!password) {
-            displayErrorMessage('login-password', 'Please enter your password.');
+            displayErrorMessage("login-password", "Please enter your password.");
             return;
         }
 
-        const data = setFetchData('post', {
+        const data = setFetchData("post", {
             email: email,
             password: password,
         });
 
         // login api 호출
-        const login_response = await fetch('/api/user/auth', data);
+        const login_response = await fetch("/api/user/auth", data);
 
         if (login_response.status === 200) {
-            location.href = '/';
+            location.href = "/";
         } else if (login_response.status === 400) {
             const errorData = await login_response.json();
-            console.log('errorData:', errorData);
+            console.log("errorData:", errorData);
             if (errorData) {
                 if (errorData.email) {
-                    displayErrorMessage('login-email', errorData.email);
+                    displayErrorMessage("login-email", errorData.email);
                 }
                 if (errorData.password) {
-                    displayErrorMessage('login-password', errorData.password);
+                    displayErrorMessage("login-password", errorData.password);
                 }
             }
         }
     }
 
     // email 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(loginEmailInput, 'Enter', login)
-    
+    setKeyForFunction(loginEmailInput, "Enter", login);
+
     // password 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(loginPasswordInput, 'Enter', login)
+    setKeyForFunction(loginPasswordInput, "Enter", login);
 
     // login submit 버튼 마우스 오버 시 효과
-    imageHover(loginSubmitBtn, '/static/img/icon/submit02.png', '/static/img/icon/submit03.png')
+    imageHover(loginSubmitBtn, "/static/img/icon/submit02.png", "/static/img/icon/submit03.png");
 
     // find email 버튼 마우스 오버 시 효과
-    const fineEmailBtn = getElFromId('find_email')
-    imageHover(fineEmailBtn, '/static/img/icon/find_email04.png', '/static/img/icon/find_email03.png')
-    
+    const fineEmailBtn = getElFromId("find_email");
+    imageHover(
+        fineEmailBtn,
+        "/static/img/icon/find_email04.png",
+        "/static/img/icon/find_email03.png"
+    );
+
     // find password 버튼 마우스 오버 시 효과
-    const finePwBtn = getElFromId('find_pw')
-    imageHover(finePwBtn, '/static/img/icon/find_pw06.png', '/static/img/icon/find_pw05.png')
+    const finePwBtn = getElFromId("find_pw");
+    imageHover(finePwBtn, "/static/img/icon/find_pw06.png", "/static/img/icon/find_pw05.png");
 
     // signup 버튼 마우스 오버 시 효과
-    const signupBtn = getElFromId('signup')
-    imageHover(signupBtn, '/static/img/icon/signup03.png', '/static/img/icon/signup02.png')
+    const signupBtn = getElFromId("signup");
+    imageHover(signupBtn, "/static/img/icon/signup03.png", "/static/img/icon/signup02.png");
 
     // signup 버튼 클릭 시, 모달 생성
-    signupBtn.addEventListener('click', openModal)
-    setKeyForFunction(signupBtn, "Enter", openModal)
+    signupBtn.addEventListener("click", openModal);
+    setKeyForFunction(signupBtn, "Enter", openModal);
 
     async function openModal() {
-        const signupModal = getElFromId('signup_modal');
-        signupModal.classList.remove('hidden');
-        loginEmailInput.value='';
-        loginPasswordInput.value='';
-        const signupEmailInput = getElFromId('signup_email');
+        const signupModal = getElFromId("signup_modal");
+        signupModal.classList.remove("hidden");
+        loginEmailInput.value = "";
+        loginPasswordInput.value = "";
+        const signupEmailInput = getElFromId("signup_email");
         signupEmailInput.focus();
     }
 });
-
-
--- applications/user/static/js/signup.js
+++ applications/user/static/js/signup.js
@@ -17,21 +17,23 @@ import {
     displayErrorMessage,
     displayPermanentErrorMessage,
     setKeyForFunction,
-} from './common.js';
+} from "./common.js";
 
-document.addEventListener('DOMContentLoaded', function() {
-
-    const signupEmailInput = getElFromId('signup_email');
-    const signupFullnameInput = getElFromId('signup_fullname');
-    const signupPhoneInput = getElFromId('signup_phone');
-    const signupPasswordInput = getElFromId('signup_password');
-    const signupPasswordConfirmInput = getElFromId('signup_password_confirm');
+document.addEventListener("DOMContentLoaded", function () {
+    const signupEmailInput = getElFromId("signup_email");
+    const signupFullnameInput = getElFromId("signup_fullname");
+    const signupPhoneInput = getElFromId("signup_phone");
+    const signupPasswordInput = getElFromId("signup_password");
+    const signupPasswordConfirmInput = getElFromId("signup_password_confirm");
 
     // Email Check
     if (signupEmailInput) {
-        signupEmailInput.addEventListener('input', debounce(() => {
-            notExistUser('exist_email', { email: signupEmailInput.value }, "signup-email");
-        }, 500));
+        signupEmailInput.addEventListener(
+            "input",
+            debounce(() => {
+                notExistUser("exist_email", { email: signupEmailInput.value }, "signup-email");
+            }, 500)
+        );
     }
 
     async function notExistUser(endpoint, dict, inputField) {
@@ -42,18 +44,21 @@ document.addEventListener('DOMContentLoaded', function() {
             if (response.status === 200) {
                 const responseData = await response.json();
                 const userExist = responseData.user_exist;
-                displayPermanentErrorMessage(userExist, inputField, `Already in use.`)
+                displayPermanentErrorMessage(userExist, inputField, `Already in use.`);
             } else {
-                console.log('API 요청 실패');
+                console.log("API request failed");
             }
         }
     }
 
     // Phone Check
     if (signupPhoneInput) {
-        signupPhoneInput.addEventListener('input', debounce(() => {
-            checkPhone('check_phone', { phone: signupPhoneInput.value });
-        }, 500));
+        signupPhoneInput.addEventListener(
+            "input",
+            debounce(() => {
+                checkPhone("check_phone", { phone: signupPhoneInput.value });
+            }, 500)
+        );
     }
 
     async function checkPhone(endpoint, dict) {
@@ -66,72 +71,93 @@ document.addEventListener('DOMContentLoaded', function() {
                 const userExist = responseData.user_exist;
                 const phoneFormat = responseData.phone_format;
                 const errorElement = getElFromSel(`.signup-phone-error`);
-                
+
                 if (userExist) {
-                    errorElement.textContent = 'Already in use.';
-                    errorElement.classList.remove('hidden');
+                    errorElement.textContent = "Already in use.";
+                    errorElement.classList.remove("hidden");
                 } else if (!phoneFormat) {
                     errorElement.textContent = 'Format: "010-1234-1234"';
-                    errorElement.classList.remove('hidden');
+                    errorElement.classList.remove("hidden");
                 } else {
-                    errorElement.classList.add('hidden');
-                    errorElement.textContent = '';
+                    errorElement.classList.add("hidden");
+                    errorElement.textContent = "";
                 }
             } else {
-                console.log('API 요청 실패');
+                console.log("API request failed");
             }
         }
     }
 
     // Password Format
     if (signupPasswordInput) {
-        signupPasswordInput.addEventListener('input', debounce(() => {
-            formatPassword(signupPasswordInput.value, "signup-password");}, 500));
+        signupPasswordInput.addEventListener(
+            "input",
+            debounce(() => {
+                formatPassword(signupPasswordInput.value, "signup-password");
+            }, 500)
+        );
     }
 
     async function formatPassword(password, inputField) {
-
         if (password) {
             const data = setFetchData("post", { password: password });
-            const response = await fetch('/api/user/format_pw', data);
+            const response = await fetch("/api/user/format_pw", data);
 
             if (response.status === 200) {
                 const responseData = await response.json();
                 const passwordCheck = responseData.password_format;
-                displayPermanentErrorMessage(!passwordCheck, inputField, "문자, 숫자, 특수문자 포함 8자리 이상으로 작성해주세요.")
+                displayPermanentErrorMessage(
+                    !passwordCheck,
+                    inputField,
+                    "Enter at least 8 chars with at least 1 letter, 1 number, 1 special char(@$!%*#?&)"
+                );
             } else {
-                console.log('API 요청 실패');
+                console.log("API request failed");
             }
         }
     }
 
     // PasswordConfirm Check - Format & Match
-    signupPasswordConfirmInput.addEventListener('input', debounce(() => {
-        formatPasswordConfirm(signupPasswordInput.value, signupPasswordConfirmInput.value, "signup-password-confirm");}, 500));
+    signupPasswordConfirmInput.addEventListener(
+        "input",
+        debounce(() => {
+            formatPasswordConfirm(
+                signupPasswordInput.value,
+                signupPasswordConfirmInput.value,
+                "signup-password-confirm"
+            );
+        }, 500)
+    );
 
     async function formatPasswordConfirm(password, password_confirm, inputField) {
-
         if (password_confirm) {
             const data = setFetchData("post", { password: password_confirm });
-            const response = await fetch('/api/user/format_pw', data);
+            const response = await fetch("/api/user/format_pw", data);
 
             if (response.status === 200) {
                 const responseData = await response.json();
                 const passwordCheck = responseData.password_format;
-                displayPermanentErrorMessage(!passwordCheck, inputField, "문자, 숫자, 특수문자 포함 8자리 이상으로 작성해주세요.")
+                displayPermanentErrorMessage(
+                    !passwordCheck,
+                    inputField,
+                    "Enter at least 8 chars with at least 1 letter, 1 number, 1 special char(@$!%*#?&)"
+                );
             } else {
-                console.log('API 요청 실패');
+                console.log("API request failed");
             }
         } else if (password && password_confirm && password != password_confirm) {
-            displayPermanentErrorMessage(password != password_confirm, inputField, "Password not matched.")
+            displayPermanentErrorMessage(
+                password != password_confirm,
+                inputField,
+                "Password not matched."
+            );
         }
     }
 
-
     // Submit Signup Form
-    const signupSubmitBtn = getElFromId('signup_btn_email');
+    const signupSubmitBtn = getElFromId("signup_btn_email");
 
-    signupSubmitBtn.addEventListener('click', signup)
+    signupSubmitBtn.addEventListener("click", signup);
 
     async function signup() {
         const email = signupEmailInput.value;
@@ -140,107 +166,104 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = signupPasswordInput.value;
         const passwordConfirm = signupPasswordConfirmInput.value;
 
-
         if (!email) {
-            displayErrorMessage('signup-email', 'Please enter your email.');
+            displayErrorMessage("signup-email", "Please enter your email.");
             return;
         }
 
         if (!fullname) {
-            displayErrorMessage('signup-fullname', 'Please enter your name.');
+            displayErrorMessage("signup-fullname", "Please enter your name.");
             return;
         }
 
         if (!phone) {
-            displayErrorMessage('signup-phone', 'Please enter your phone.');
+            displayErrorMessage("signup-phone", "Please enter your phone.");
             return;
         }
 
         if (!password) {
-            displayErrorMessage('signup-password', 'Please enter your password.');
+            displayErrorMessage("signup-password", "Please enter your password.");
             return;
         }
 
         if (!passwordConfirm) {
-            displayErrorMessage('signup-password-confirm', 'Please enter your password again.');
+            displayErrorMessage("signup-password-confirm", "Please enter your password again.");
             return;
         }
 
-        const data = setFetchData('post', {
+        const data = setFetchData("post", {
             email: email,
             phone: phone,
             fullname: fullname,
             password: password,
-            password_confirm: passwordConfirm
+            password_confirm: passwordConfirm,
         });
 
         // signup api 호출
-        const signup_response = await fetch('/api/user/signup', data);
+        const signup_response = await fetch("/api/user/signup", data);
 
         if (signup_response.status === 200) {
-            location.href = '/';
+            location.href = "/";
         } else if (signup_response.status === 400) {
             const errorData = await signup_response.json();
-            console.log('errorData:', errorData);
+            console.log("errorData:", errorData);
             if (errorData) {
                 if (errorData.email) {
-                    displayErrorMessage('signup-email', errorData.email);
+                    displayErrorMessage("signup-email", errorData.email);
                 }
                 if (errorData.fullname) {
-                    displayErrorMessage('signup-fullname', errorData.fullname);
+                    displayErrorMessage("signup-fullname", errorData.fullname);
                 }
                 if (errorData.phone) {
-                    displayErrorMessage('signup-phone', errorData.phone);
+                    displayErrorMessage("signup-phone", errorData.phone);
                 }
                 if (errorData.password) {
-                    displayErrorMessage('signup-password', errorData.password);
+                    displayErrorMessage("signup-password", errorData.password);
                 }
                 if (errorData.password_confirm) {
-                    displayErrorMessage('signup-password', errorData.password_confirm);
+                    displayErrorMessage("signup-password", errorData.password_confirm);
                 }
             }
         }
     }
 
     // email 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(signupEmailInput, 'Enter', signup)
-    
+    setKeyForFunction(signupEmailInput, "Enter", signup);
+
     // fullname 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(signupFullnameInput, 'Enter', signup)
+    setKeyForFunction(signupFullnameInput, "Enter", signup);
 
     // phone 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(signupPhoneInput, 'Enter', signup)
-    
+    setKeyForFunction(signupPhoneInput, "Enter", signup);
+
     // password 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(signupPasswordInput, 'Enter', signup)
-    
+    setKeyForFunction(signupPasswordInput, "Enter", signup);
+
     // password_confirm 입력 칸에서 Enter 키 입력 시 폼 제출
-    setKeyForFunction(signupPasswordConfirmInput, 'Enter', signup)
+    setKeyForFunction(signupPasswordConfirmInput, "Enter", signup);
 
     // signup submit 버튼 마우스 오버 시 효과
-    imageHover(signupSubmitBtn, '/static/img/icon/submit02.png', '/static/img/icon/submit03.png')
+    imageHover(signupSubmitBtn, "/static/img/icon/submit02.png", "/static/img/icon/submit03.png");
 
     // x 버튼 클릭 시 모달창 닫기
-    const closeModalBtn = getElFromId('signup_btn_close')
+    const closeModalBtn = getElFromId("signup_btn_close");
     if (closeModalBtn) {
-        closeModalBtn.addEventListener('click', closeModal)
+        closeModalBtn.addEventListener("click", closeModal);
     }
 
     // Esc 입력 시 모달창 닫기
-    setKeyForFunction(window, 'Escape', closeModal)
+    setKeyForFunction(window, "Escape", closeModal);
 
     async function closeModal() {
-        const signupModal = getElFromId('signup_modal');
-        signupModal.classList.add('hidden');
-        signupEmailInput.value = '';
-        signupFullnameInput.value = '';
-        signupPhoneInput.value = '';
-        signupPasswordInput.value = '';
-        signupPasswordConfirmInput.value = '';
+        const signupModal = getElFromId("signup_modal");
+        signupModal.classList.add("hidden");
+        signupEmailInput.value = "";
+        signupFullnameInput.value = "";
+        signupPhoneInput.value = "";
+        signupPasswordInput.value = "";
+        signupPasswordConfirmInput.value = "";
 
-        const loginEmailInput = getElFromId('login_email');
+        const loginEmailInput = getElFromId("login_email");
         loginEmailInput.focus();
     }
 });
-
-
