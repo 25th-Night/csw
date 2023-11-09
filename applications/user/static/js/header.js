98682fe81dabe948b60c75773a71b3ab601167a8
25th-Night <brown_eyed87@naver.com>
2023-11-09 13:40:00 +0900

    feat)header: admin 버튼 클릭 관련 이벤트 추가

--- applications/user/static/js/header.js
+++ applications/user/static/js/header.js
@@ -13,10 +13,22 @@ import {
     setFetchData,
     redirectLogin,
     imageHover,
+    getShortenerURL,
+    getCurrentPath,
+    changeLogoText,
 } from "./common.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+    // Change logo text
+    changeLogoText("/url/", "URL");
+
+    // Shortener Url Service URL
+    const ShortenerUrl = getShortenerURL();
+
     const adminBtn = getElFromId("header_admin_btn");
+    const adminBtnWrap = getElFromId("admin_btn_wrap");
+    const adminUserBtn = getElFromId("header_admin_user_btn");
+    const adminUrlBtn = getElFromId("header_admin_url_btn");
     const urlBtn = getElFromId("header_url_btn");
     const jdBtn = getElFromId("header_jd_btn");
     const loginBtn = getElFromId("header_login_btn");
@@ -26,11 +38,59 @@ document.addEventListener("DOMContentLoaded", function () {
     imageHover(urlBtn, "/static/img/icon/url06.png", "/static/img/icon/url05.png");
     imageHover(jdBtn, "/static/img/icon/job-post02.png", "/static/img/icon/job-post01.png");
 
+    // admin 버튼 마우스 오버 시 효과 + 클릭 시 Admin Wrap 태그 보이기
     if (adminBtn) {
-        // admin 버튼 마우스 오버 시 효과
         imageHover(adminBtn, "/static/img/icon/admin03.png", "/static/img/icon/admin02.png");
-        adminBtn.addEventListener("click", () => {
+        // admin 메뉴 요소 가져오기
+        const adminMenu = getElFromSel(".admin-menu");
+        console.log(adminMenu);
+
+        // 드롭다운 버튼을 클릭할 때 드롭다운 메뉴의 표시/숨김을 토글
+        adminBtn.addEventListener("click", function () {
+            console.log("click adminBtn");
+            if (adminMenu.classList.contains("hidden")) {
+                adminMenu.classList.remove("hidden");
+            } else {
+                adminMenu.classList.add("hidden");
+            }
+
+            // 다른 곳을 클릭할 때 드롭다운 메뉴를 숨김
+            document.addEventListener("click", function (event) {
+                if (adminMenu && ![adminMenu, adminBtn].includes(event.target)) {
+                    adminMenu.classList.add("hidden");
+                }
+            });
+
+            // 드롭다운 메뉴 내부 요소 클릭 시 이벤트가 상위로 전파되지 않도록 막음
+            adminMenu.addEventListener("click", function (event) {
+                event.stopPropagation();
+            });
+        });
+    }
+
+    // User Admin 버튼 마우스 오버 시 효과 + 클릭 시 User 앱 admin 페이지로 이동
+    if (adminUserBtn) {
+        imageHover(
+            adminUserBtn,
+            "/static/img/icon/admin_user03.png",
+            "/static/img/icon/admin_user02.png"
+        );
+        adminUserBtn.addEventListener("click", function () {
             window.open("/admin/", "_blank");
+            this.parentNode.parentNode.classList.add("hidden");
+        });
+    }
+
+    // Url Admin 버튼 마우스 오버 시 효과 + 클릭 시 Url 앱 admin 페이지로 이동
+    if (adminUrlBtn) {
+        imageHover(
+            adminUrlBtn,
+            "/static/img/icon/admin_url03.png",
+            "/static/img/icon/admin_url02.png"
+        );
+        adminUrlBtn.addEventListener("click", function () {
+            window.open(`${ShortenerUrl}/admin/`, "_blank");
+            this.parentNode.parentNode.classList.add("hidden");
         });
     }
 
