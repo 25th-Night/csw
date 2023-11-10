cb00713145072933b41ac5fb47d165c5c267a67b
25th-Night <brown_eyed87@naver.com>
2023-11-10 15:34:43 +0900

    feat)urls: 생성한 뷰의 url 추가, url 위치 조정

--- applications/shortener/url/urls.py
+++ applications/shortener/url/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
 
-from url.views import ShortenedUrlDetailView, ShortenedUrlView, UrlRedirectView
+from url.views import (
+    ShortenedUrlDetailViaIdView,
+    ShortenedUrlDetailViaUrlView,
+    ShortenedUrlView,
+    UrlRedirectView,
+    RefreshAccessCodeView,
+)
 
 
 urlpatterns = [
-    path("shortener/<int:pk>", ShortenedUrlDetailView.as_view(), name="url_detail"),
+    path(
+        "shortener/<int:pk>/refresh",
+        RefreshAccessCodeView.as_view(),
+        name="access_code_refresh",
+    ),
+    path(
+        "shortener/<int:pk>", ShortenedUrlDetailViaIdView.as_view(), name="url_detail"
+    ),
+    path(
+        "shortener/<str:prefix>/<str:url>",
+        ShortenedUrlDetailViaUrlView.as_view(),
+        name="url_detail_via_url",
+    ),
     path("shortener/", ShortenedUrlView.as_view(), name="url_list"),
     path("<str:prefix>/<str:url>", UrlRedirectView.as_view(), name="redirect"),
 ]
