d588e96f5095c941c0e2ec1e776ae58d469c1dde
25th-Night <brown_eyed87@naver.com>
2023-11-10 15:36:18 +0900

    feat)url: private, secret url에 대한 뷰 함수 추가

--- applications/user/rendering/views/url.py
+++ applications/user/rendering/views/url.py
@@ -1,5 +1,10 @@
+from urllib.parse import urlencode
+from django.conf import settings
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
+import requests
 
 from common.permissions import LoginRequired
 from common.data import LICENSE
@@ -12,4 +17,74 @@ class UrlView(LoginRequired, TemplateView):
         user = request.user
         available_url_cnt = LICENSE.get(user.url.license).get("max_link_cnt")
         kwargs["available_url_cnt"] = available_url_cnt
+        return super().get(request, *args, **kwargs)
+
+
+class PrivateUrlView(TemplateView):
+    template_name = "url/permission/private.html"
+
+    def get(self, request, *args, **kwargs):
+        prefix = self.request.GET.get("prefix")
+        url = self.request.GET.get("url")
+        url_path = f"/{prefix}/{url}"
+
+        kwargs["url_path"] = url_path
+
+        get_url = f"{settings.REQUEST_URL_DOMAIN}/shortener{url_path}"
+        response = requests.get(get_url, verify=False)
+        if response.status_code == 200:
+            response_url = response.json()
+            if response_url.get("access") == 1:
+                target_url = response_url.get("target_url")
+                if not target_url.startswith("https://") and not target_url.startswith(
+                    "http://"
+                ):
+                    target_url = "https://" + target_url
+                return redirect(target_url)
+
+            elif response_url.get("access") == 3:
+                secret_url = reverse("url_secret")
+                query_params = {"prefix": prefix, "url": url}
+                url_with_query = f"{secret_url}?{urlencode(query_params)}"
+                return redirect(url_with_query)
+            if request.user and request.user.id != response_url.get("creator_id"):
+                response_url.pop("access_code")
+            kwargs["url"] = response_url
+        else:
+            kwargs["msg"] = "Url not found"
+
+        return super().get(request, *args, **kwargs)
+
+
+class SecretUrlView(TemplateView):
+    template_name = "url/permission/secret.html"
+
+    def get(self, request, *args, **kwargs):
+        prefix = self.request.GET.get("prefix")
+        url = self.request.GET.get("url")
+        url_path = f"/{prefix}/{url}"
+
+        kwargs["url_path"] = url_path
+
+        get_url = f"{settings.REQUEST_URL_DOMAIN}/shortener{url_path}"
+        response = requests.get(get_url, verify=False)
+        if response.status_code == 200:
+            response_url = response.json()
+            if response_url.get("access") == 1:
+                target_url = response_url.get("target_url")
+                if not target_url.startswith("https://") and not target_url.startswith(
+                    "http://"
+                ):
+                    target_url = "https://" + target_url
+                return redirect(target_url)
+
+            elif response_url.get("access") == 2:
+                private_url = reverse("url_private")
+                query_params = {"prefix": prefix, "url": url}
+                url_with_query = f"{private_url}?{urlencode(query_params)}"
+                return redirect(url_with_query)
+            kwargs["url"] = response_url
+        else:
+            kwargs["msg"] = "Url not found"
+
         return super().get(request, *args, **kwargs)
