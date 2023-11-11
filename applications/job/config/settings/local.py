f32376f4de404165183259c75bc09bec63e15fb0
25th-Night <brown_eyed87@naver.com>
2023-11-11 13:38:13 +0900

    feat)settings: settings 환경별 분리

---
+++ applications/job/config/settings/local.py
@@ -0,0 +1,49 @@
+from .base import *
+
+
+SERVICE_DOMAIN = f"http://{SERVICE_HOST}:83"
+USER_DOMAIN = f"http://{USER_SERVICE_HOST}:81"
+
+ALLOWED_HOSTS += [
+    "job",
+]
+
+CSRF_ALLOWED_ORIGINS += [
+    USER_DOMAIN,
+    SERVICE_DOMAIN,
+    "http://job:8000",
+]
+
+CSRF_TRUSTED_ORIGINS += [
+    USER_DOMAIN,
+    SERVICE_DOMAIN,
+    "http://job:8000",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
+        "OPTIONS": {"options": "-c search_path=job,public"},
+    }
+}
+
+
+# CORS settings
+
+CORS_ORIGIN_WHITELIST += [
+    USER_DOMAIN,
+    SERVICE_DOMAIN,
+]
+CORS_ALLOWED_ORIGINS += [
+    USER_DOMAIN,
+    SERVICE_DOMAIN,
+]
+
+
+# Request DOMAIN
+REQUEST_USER_DOMAIN = "http://user:8000"
---
+++ applications/job/config/settings/staging.py
@@ -0,0 +1,65 @@
+import os
+
+from .base import *
+
+
+DEBUG = True
+
+
+SERVICE_HOST = os.getenv("JOB_SERVICE_HOST")
+USER_SERVICE_HOST = os.getenv("USER_SERVICE_HOST")
+
+SERVICE_DOMAIN = f"https://{SERVICE_HOST}"
+USER_DOMAIN = f"https://{USER_SERVICE_HOST}"
+
+ALLOWED_HOSTS += [
+    SERVICE_HOST,
+    USER_SERVICE_HOST,
+]
+
+CSRF_ALLOWED_ORIGINS += [
+    SERVICE_DOMAIN,
+    USER_DOMAIN,
+]
+
+CSRF_TRUSTED_ORIGINS += [
+    SERVICE_DOMAIN,
+    USER_DOMAIN,
+]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
+        "OPTIONS": {"options": "-c search_path=job,public"},
+    }
+}
+
+
+# CORS settings
+
+CORS_ORIGIN_WHITELIST += [
+    SERVICE_DOMAIN,
+    USER_DOMAIN,
+]
+CORS_ALLOWED_ORIGINS += [
+    SERVICE_DOMAIN,
+    USER_DOMAIN,
+]
+
+
+# COOKIE settings
+
+# domain
+DOMAIN = USER_SERVICE_HOST
+
+# Request DOMAIN
+REQUEST_USER_DOMAIN = USER_DOMAIN
+
+# secure
+SECURE = True
