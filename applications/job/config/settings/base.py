4cd04c0a9eded2b9e37e291dc923e9416431a054
25th-Night <brown_eyed87@naver.com>
2023-11-11 13:44:53 +0900

    update)base: CorsMiddleware 추가

--- applications/job/config/settings/base.py
+++ applications/job/config/settings/base.py
@@ -82,6 +82,7 @@ INSTALLED_APPS += [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
