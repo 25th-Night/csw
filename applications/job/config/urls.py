bcdeef5244a8219b1026639869f93701a7b4c5c6
25th-Night <brown_eyed87@naver.com>
2023-11-13 09:29:51 +0900

    feat)url: 각 앱의 url을 등록

--- applications/job/config/urls.py
+++ applications/job/config/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("crawler/", include("crawler.urls")),
+    path("", include("job.urls")),
 ]
 
 
