dbed8fb6fd0436626c1bb6e0c17ce2bee23bcc4d
25th-Night <brown_eyed87@naver.com>
2023-11-14 20:28:59 +0900

    feat)urls: 생성한 뷰의 url 등록

--- applications/job/crawler/urls.py
+++ applications/job/crawler/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 
+from crawler.views import (
+    RecruitDetailView,
+    RecruitView,
+)
 
-urlpatterns = []
+
+urlpatterns = [
+    path("recruits/", RecruitView.as_view(), name="recruits"),
+    path("recruits/<int:url>", RecruitDetailView.as_view(), name="recruit"),
+]
