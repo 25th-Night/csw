fba1506d311146c8e2f41a68c1df10950931c2a9
25th-Night <brown_eyed87@naver.com>
2023-11-14 22:16:15 +0900

    feat)urls: 생성한 뷰의 url 등록

--- applications/job/job/urls.py
+++ applications/job/job/urls.py
@@ -1,4 +1,20 @@
 from django.urls import path
 
+from job.views import (
+    CountryView,
+    DetailRegionView,
+    CategoryView,
+    GroupView,
+    RegionView,
+    RecruitView,
+)
 
-urlpatterns = []
+
+urlpatterns = [
+    path("groups", GroupView.as_view(), name="groups"),
+    path("categories", CategoryView.as_view(), name="category"),
+    path("countries", CountryView.as_view(), name="country"),
+    path("regions", RegionView.as_view(), name="regions"),
+    path("detail_regions", DetailRegionView.as_view(), name="detail_region"),
+    path("recruits", RecruitView.as_view(), name="recruits"),
+]
