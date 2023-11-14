28b5a5f1551fc8e1bcb43c4dc567d4e21812a107
25th-Night <brown_eyed87@naver.com>
2023-11-14 20:26:02 +0900

    feat)serializers: CrawlingRecruitSerializer 추가

---
+++ applications/job/crawler/serializers.py
@@ -0,0 +1,75 @@
+from rest_framework import serializers
+
+from taggit.serializers import TagListSerializerField
+
+from job.models import (
+    Group,
+    Category,
+    Country,
+    Region,
+    DetailRegion,
+    Company,
+    Site,
+    Skill,
+    Recruit,
+)
+
+
+class CrawlingRecruitSerializer(serializers.Serializer):
+    site_name = serializers.CharField()
+    job_category_name = serializers.CharField()
+    detail_region_name = serializers.CharField()
+    company_name = serializers.CharField()
+    company_tags = serializers.ListField()
+    skills = serializers.ListField()
+    min_career = serializers.IntegerField()
+    url_id = serializers.CharField()
+    position = serializers.CharField()
+    description = serializers.CharField()
+    task = serializers.CharField()
+    requirement = serializers.CharField()
+    preference = serializers.CharField()
+    benefit = serializers.CharField()
+    workplace = serializers.CharField()
+    status = serializers.BooleanField()
+
+    def get_or_create(self, validated_data):
+        site_name = validated_data.pop("site_name")
+        job_category_name = validated_data.pop("job_category_name")
+        detail_region_name = validated_data.pop("detail_region_name")
+        company_name = validated_data.pop("company_name")
+        company_tags = validated_data.pop("company_tags")
+        skills = validated_data.pop("skills")
+        min_career = validated_data.pop("min_career")
+
+        site = Site.objects.get(name=site_name)
+        category = Category.objects.get(name=job_category_name)
+        detail_region = DetailRegion.objects.get(name=detail_region_name)
+        company, company_created = Company.objects.get_or_create(
+            name=company_name, detail_region=detail_region
+        )
+        if company_created:
+            company.tags.add(*company_tags)
+        skills = [
+            Skill.objects.get_or_create(name=skill_name)[0] for skill_name in skills
+        ]
+
+        recruit, created = Recruit.objects.get_or_create(
+            site=site,
+            detail_region=detail_region,
+            company=company,
+            **validated_data,
+            defaults={"min_career": min_career},
+        )
+        status = "existed"
+        if created:
+            recruit.categories.add(category)
+            recruit.skills.add(*skills)
+            status = "created"
+        else:
+            if recruit.min_career != min(recruit.min_career, min_career):
+                recruit.min_career = min(recruit.min_career, min_career)
+                recruit.save()
+                status = "modified"
+
+        return recruit, status
