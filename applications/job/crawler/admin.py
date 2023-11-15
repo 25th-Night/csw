18bf84d34ed8a4ad5693905b38d6d5ff46d893cf
25th-Night <brown_eyed87@naver.com>
2023-11-15 11:30:42 +0900

    feat)admin: crawling 모델의 마이그레이션 추가

--- applications/job/crawler/admin.py
+++ applications/job/crawler/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
 
-# Register your models here.
+from crawler.models import Crawling, CrawlingCategory, CrawlingSkill
+
+
+@admin.register(Crawling)
+class CrawlingAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user_id",
+        "site",
+        "min_career",
+        "group",
+        "country",
+        "region",
+        "detail_region",
+        "display_categories",
+        "display_skills",
+    ]
+    list_filter = ["created_at", "categories__name", "skills__name"]
+    search_fields = [
+        "site",
+        "user_id",
+        "min_career",
+        "group",
+        "category",
+        "country",
+        "region",
+        "detail_region",
+        "categories__name",
+        "skills__name",
+    ]
+
+    @admin.display(description="스킬")
+    def display_skills(self, obj):
+        return ", ".join(skill.name for skill in obj.skills.all())
+
+    @admin.display(description="카테고리")
+    def display_categories(self, obj):
+        return ", ".join(category.name for category in obj.categories.all())
+
+
+@admin.register(CrawlingCategory)
+class CrawlingCategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "crawling",
+        "category",
+    ]
+
+
+@admin.register(CrawlingSkill)
+class CrawlingSkillAdmin(admin.ModelAdmin):
+    list_display = [
+        "crawling",
+        "skill",
+    ]
