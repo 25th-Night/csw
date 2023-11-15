d07584d2878739c1a13a844adcaf8277fd04175c
25th-Night <brown_eyed87@naver.com>
2023-11-15 11:30:17 +0900

    feat)models: Crawling 모델 및 중간모델 생성

--- applications/job/crawler/models.py
+++ applications/job/crawler/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+from job.models import Category, Country, DetailRegion, Group, Region, Site, Skill
+from common.models import CommonModel
+
+
+class Crawling(CommonModel):
+    user_id = models.IntegerField(verbose_name="이용자 ID")
+    site = models.ForeignKey(
+        Site,
+        verbose_name="사이트",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="crawling",
+    )
+    min_career = models.IntegerField(verbose_name="최소 경력년수", null=True)
+    group = models.ForeignKey(
+        Group,
+        verbose_name="그룹",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="crawling",
+    )
+    categories = models.ManyToManyField(
+        Category,
+        through="CrawlingCategory",
+        verbose_name="카테고리",
+        related_name="crawling",
+    )
+    country = models.ForeignKey(
+        Country,
+        verbose_name="국가",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="crawling",
+    )
+    region = models.ForeignKey(
+        Region,
+        verbose_name="지역",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="crawling",
+    )
+    detail_region = models.ForeignKey(
+        DetailRegion,
+        verbose_name="지역",
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="crawling",
+    )
+    skills = models.ManyToManyField(
+        Skill, through="CrawlingSkill", verbose_name="스킬", related_name="crawling"
+    )
+
+    def __str__(self):
+        return f"Crawling No.{self.id}"
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["-created_at"]),
+        ]
+        ordering = ["-created_at"]
+
+
+class CrawlingCategory(models.Model):
+    crawling = models.ForeignKey(Crawling, verbose_name="크롤링", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, verbose_name="카테고리", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(verbose_name="생성일", auto_now_add=True)
+
+    class Meta:
+        db_table = "through_crawling_category"
+
+
+class CrawlingSkill(models.Model):
+    crawling = models.ForeignKey(Crawling, verbose_name="크롤링", on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, verbose_name="스킬", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name="생성일", auto_now_add=True)
+
+    class Meta:
+        db_table = "through_crawling_skill"
