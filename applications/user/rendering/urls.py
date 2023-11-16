0082dc6e83d48334a8e9076d9de2595d7c23ff1d
25th-Night <brown_eyed87@naver.com>
2023-11-16 21:02:16 +0900

    feat)urls: 생성한 뷰의 url 추가

--- applications/user/rendering/urls.py
+++ applications/user/rendering/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from rendering.views.index import IndexView
 from rendering.views.user import LoginView
 from rendering.views.url import PrivateUrlView, SecretUrlView, UrlView
+from rendering.views.job import JobView
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
-    path("url/", UrlView.as_view(), name="url"),
     path("url/private", PrivateUrlView.as_view(), name="url_private"),
     path("url/secret", SecretUrlView.as_view(), name="url_secret"),
+    path("url/", UrlView.as_view(), name="url"),
+    path("job/", JobView.as_view(), name="job"),
 ]
