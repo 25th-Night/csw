d94901c46771c086a98edd7ad6a861135d32dd14
25th-Night <brown_eyed87@naver.com>
2023-11-16 21:00:01 +0900

    feat)urls: 생성한 뷰의 url 추가

--- applications/job/job/urls.py
+++ applications/job/job/urls.py
@@ -5,16 +5,20 @@ from job.views import (
     DetailRegionView,
     CategoryView,
     GroupView,
+    RecruitDetailView,
     RegionView,
     RecruitView,
+    SiteView,
 )
 
 
 urlpatterns = [
+    path("sites", SiteView.as_view(), name="sites"),
     path("groups", GroupView.as_view(), name="groups"),
     path("categories", CategoryView.as_view(), name="category"),
     path("countries", CountryView.as_view(), name="country"),
     path("regions", RegionView.as_view(), name="regions"),
     path("detail_regions", DetailRegionView.as_view(), name="detail_region"),
     path("recruits", RecruitView.as_view(), name="recruits"),
+    path("recruits/<int:pk>", RecruitDetailView.as_view(), name="recruit"),
 ]
