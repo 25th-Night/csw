d97a42508f92e4e42adcb2f662093e2e1e965c8b
25th-Night <brown_eyed87@naver.com>
2023-11-16 21:01:42 +0900

    feat)data: JOB_LICENSE 추가

--- applications/user/common/data.py
+++ applications/user/common/data.py
@@ -24,3 +24,22 @@ URL_LICENSE = {
         "allow_alias": True,
     },
 }
+
+JOB_LICENSE = {
+    1: {
+        "grade": "FREE",
+        "daily_crawling_limit": 1,
+    },
+    2: {
+        "grade": "BASIC",
+        "daily_crawling_limit": 5,
+    },
+    3: {
+        "grade": "PREMIUM",
+        "daily_crawling_limit": 10,
+    },
+    4: {
+        "grade": "MASTER",
+        "daily_crawling_limit": 30,
+    },
+}
