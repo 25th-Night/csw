5dbb7ae7de84627d0da31c0e17851dcb36ee5c90
25th-Night <brown_eyed87@naver.com>
2023-11-18 10:10:41 +0900

    update)filter: min_career 필터 로직 수정

--- applications/job/job/filters.py
+++ applications/job/job/filters.py
@@ -22,6 +22,9 @@ class RegionFilter(filters.FilterSet):
 
 class DetailRegionFilter(filters.FilterSet):
     region_id = filters.NumberFilter(field_name="region", lookup_expr="exact")
+    country_id = filters.NumberFilter(
+        field_name="region__country__id", lookup_expr="exact"
+    )
 
     class Meta:
         model = DetailRegion
@@ -43,6 +46,8 @@ class RecruitFilter(filters.FilterSet):
     company_tag_ids = filters.CharFilter(method="filter_by_company_tag_ids")
     category_ids = filters.CharFilter(method="filter_by_category_ids")
     group_id = filters.CharFilter(method="filter_by_group_id")
+    min_career = filters.NumberFilter(field_name="min_career", lookup_expr="exact")
+    # min_career = filters.CharFilter(method="filter_by_min_career")
 
     class Meta:
         model = Recruit
@@ -55,6 +60,7 @@ class RecruitFilter(filters.FilterSet):
             "company_tag_ids",
             "category_ids",
             "group_id",
+            "min_career",
         ]
 
     def filter_by_skill_ids(self, queryset, name, value):
@@ -62,22 +68,36 @@ class RecruitFilter(filters.FilterSet):
         result = queryset
 
         for skill in skills:
-            result: QuerySet = result.filter(skills__name__iexact=skill)
+            result: QuerySet = result.filter(skills__id__iexact=skill)
 
         return result
 
     def filter_by_company_tag_ids(self, queryset, name, value):
-        company_tags = value.split(",")
+        company_tag_ids = [
+            int(company_tag_id)
+            for company_tag_id in value.split(",")
+            if company_tag_id.isdigit()
+        ]
+        print(f"company_tag_ids:{company_tag_ids}")
         result = queryset
 
-        for company_tag in company_tags:
-            result: QuerySet = result.filter(company__tags__name__iexact=company_tag)
+        for company_tag_id in company_tag_ids:
+            result: QuerySet = result.filter(company__tags__id__iexact=company_tag_id)
 
         return result
 
     def filter_by_category_ids(self, queryset, name, value):
-        category_ids = [int(cat_id) for cat_id in value.split(",") if cat_id.isdigit()]
+        category_ids = [
+            int(category_id)
+            for category_id in value.split(",")
+            if category_id.isdigit()
+        ]
         return queryset.filter(categories__id__in=category_ids)
 
     def filter_by_group_id(self, queryset, name, value):
         return queryset.filter(categories__group__id=value)
+
+    # def filter_by_min_career(self, queryset, name, value):
+    #     result = queryset
+
+    #     return queryset.filter(min_career__gte=int(value))
