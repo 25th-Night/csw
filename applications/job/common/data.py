ca478d536df7244fdeafdb00e50915adc70eaf24
25th-Night <brown_eyed87@naver.com>
2023-11-18 10:07:39 +0900

    feat)data: MIN_CAREER_TYPE 추가

--- applications/job/common/data.py
+++ applications/job/common/data.py
@@ -16,3 +16,18 @@ JOB_LICENSE = {
         "daily_crawling_limit": 30,
     },
 }
+
+
+MIN_CAREER_TYPE = [
+    (0, "newcomer"),
+    (1, "1 year ↑"),
+    (2, "2 year ↑"),
+    (3, "3 year ↑"),
+    (4, "4 year ↑"),
+    (5, "5 year ↑"),
+    (6, "6 year ↑"),
+    (7, "7 year ↑"),
+    (8, "8 year ↑"),
+    (9, "9 year ↑"),
+    (10, "10 year ↑"),
+]
