64f6c21d329adb398631748e2990680555a7b872
25th-Night <brown_eyed87@naver.com>
2023-11-20 15:23:28 +0900

    update) 정적 데이터 크롤링 - db에 없을 때만 실행

--- applications/job/crawler/management/commands/01_job_group.py
+++ applications/job/crawler/management/commands/01_job_group.py
@@ -21,33 +21,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("hello, it's custom command file")
 
-        # set Driver
-        chrome = Chrome()
-        driver = chrome.driver
-        wait = chrome.wait
+        if not Group.objects.exists():
+            # set Driver
+            chrome = Chrome()
+            driver = chrome.driver
+            wait = chrome.wait
 
-        url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.latest_order&years=-1&locations=all"
+            url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.latest_order&years=-1&locations=all"
 
-        driver.get(url)
-        print("크롤링을 시작합니다.")
+            driver.get(url)
+            print("크롤링을 시작합니다.")
 
-        # 직업 분류 열기
-        find_visible(wait, "button[class*=JobGroup_]").click()
-        # 직업 분류 목록
-        job_groups = finds_present(
-            driver, wait, "section[class*=JobGroupOverlay_] li a"
-        )
+            # 직업 분류 열기
+            find_visible(wait, "button[class*=JobGroup_]").click()
+            # 직업 분류 목록
+            job_groups = finds_present(
+                driver, wait, "section[class*=JobGroupOverlay_] li a"
+            )
 
-        for i, job_group in enumerate(job_groups):
-            if not i:
-                job_group.send_keys(Keys.ARROW_DOWN)
-            job_group_name = job_group.text
-            job_group_id = job_group.get_attribute("href").split("/")[-1]
-            print(i, job_group_name, job_group_id)
+            for i, job_group in enumerate(job_groups):
+                if not i:
+                    job_group.send_keys(Keys.ARROW_DOWN)
+                job_group_name = job_group.text
+                job_group_id = job_group.get_attribute("href").split("/")[-1]
+                print(i, job_group_name, job_group_id)
 
-            Group.objects.get_or_create(name=job_group_name, index=job_group_id)
+                Group.objects.get_or_create(name=job_group_name, index=job_group_id)
 
-        Site.objects.get_or_create(name="Wanted")
+            Site.objects.get_or_create(name="Wanted")
 
-        driver.quit()
-        print("크롤링을 종료합니다.")
+            driver.quit()
+            print("크롤링을 종료합니다.")
+        else:
+            print("데이터가 존재하기 때문에 크롤링을 진행하지 않습니다.")
--- applications/job/crawler/management/commands/02_job_category.py
+++ applications/job/crawler/management/commands/02_job_category.py
@@ -13,6 +13,7 @@ from common.utils import (
     Chrome,
     find_visible,
     finds_present,
+    finds_visible,
 )
 
 
@@ -29,81 +30,90 @@ class Command(BaseCommand):
 
         print("hello, it's custom command file")
 
-        # set Driver
-        chrome = Chrome()
-        driver = chrome.driver
-        wait = chrome.wait
-        short_wait = chrome.short_wait
+        if not Category.objects.exists():
+            # set Driver
+            chrome = Chrome()
+            driver = chrome.driver
+            wait = chrome.wait
+            short_wait = chrome.short_wait
 
-        url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.latest_order&years=-1&locations=all"
+            url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.latest_order&years=-1&locations=all"
 
-        delay = 1
-        retry = 0
-        while True:
-            try:
-                driver.get(url)
-                print("크롤링을 시작합니다.")
+            delay = 1
+            retry = 0
+            while True:
+                try:
+                    driver.get(url)
+                    print("크롤링을 시작합니다.")
 
-                # 회원가입/로그인 클릭
-                find_visible(short_wait, "button[data-gnb-kind=signupLogin]").click()
+                    # 회원가입/로그인 클릭
+                    find_visible(wait, "button[data-gnb-kind=signupLogin]").click()
 
-                # 아이디 입력 후 Enter
-                find_visible(wait, "input[type=email]").send_keys(
-                    os.getenv("WANTED_ID") + "\n"
-                )
-                # PW 입력 후 Enter
-                find_visible(wait, "input[type=password]").send_keys(
-                    os.getenv("WANTED_PW") + "\n"
-                )
+                    # 아이디 입력 후 Enter
+                    find_visible(wait, "input[type=email]").send_keys(
+                        os.getenv("WANTED_ID") + "\n"
+                    )
+                    # PW 입력 후 Enter
+                    find_visible(wait, "input[type=password]").send_keys(
+                        os.getenv("WANTED_PW") + "\n"
+                    )
 
-                # 직업 분류 열기
-                find_visible(wait, "button[class*=JobGroup_]").click()
-                # 직업 분류 목록
-                job_groups = finds_present(
-                    driver, wait, "section[class*=JobGroupOverlay_] li a"
-                )
-
-                len_jg = len(job_groups)
-
-                find_visible(wait, "button[class*=JobGroup_]").click()
-
-                for i in range(0, len_jg):
                     # 직업 분류 열기
                     find_visible(wait, "button[class*=JobGroup_]").click()
                     # 직업 분류 목록
                     job_groups = finds_present(
                         driver, wait, "section[class*=JobGroupOverlay_] li a"
                     )
-                    job_group = job_groups[i]
-                    if i >= 10:
-                        job_groups[0].send_keys(Keys.ARROW_DOWN)
-                        time.sleep(delay)
-                    job_group_name = job_group.text
-                    job_group_id = job_group.get_attribute("href").split("/")[-1]
 
-                    time.sleep(delay)
-                    job_group.click()
+                    len_jg = len(job_groups)
 
-                    driver.execute_script("window.scrollTo(0, 0);")
+                    find_visible(wait, "button[class*=JobGroup_]").click()
 
-                    # 직무 분류 열기
-                    find_visible(wait, "button[class*=JobCategory_]").click()
-                    # 직무 분류 목록
-                    job_categories = finds_present(
-                        driver, wait, "button[class*=JobCategoryItem_]"
-                    )
-                    for j, job_category in enumerate(job_categories):
-                        print(i, job_group_name, j, job_category.text)
-                        group = Group.objects.get(name=job_group_name)
-                        Category.objects.get_or_create(
-                            group=group, name=job_category.text
+                    for i in range(0, len_jg):
+                        # 직업 분류 열기
+                        find_visible(short_wait, "button[class*=JobGroup_]").click()
+                        # 직업 분류 목록
+                        job_groups = finds_present(
+                            driver, wait, "section[class*=JobGroupOverlay_] li a"
                         )
-                driver.quit()
-                print("크롤링을 종료합니다.")
-                break
-            except:
-                delay += 0.5
-                retry += 1
+                        job_group = job_groups[i]
+                        if i >= 10:
+                            job_groups[0].send_keys(Keys.ARROW_DOWN)
+                            time.sleep(delay)
+                        job_group_name = job_group.text
+                        job_group_id = job_group.get_attribute("href").split("/")[-1]
 
-                if retry == 3:
-                    raise Exception("3회 재시도에도 실패하였습니다.")
+                        time.sleep(delay)
+                        job_group.click()
+
+                        driver.execute_script("window.scrollTo(0, 0);")
+
+                        # 직무 분류 열기
+                        find_visible(wait, "button[class*=JobCategory_]").click()
+                        # 직무 분류 목록
+                        job_categories = finds_present(
+                            driver, wait, "button[class*=JobCategoryItem_]"
+                        )
+                        for j, job_category in enumerate(job_categories):
+                            print(i, job_group_name, j, job_category.text)
+                            group = Group.objects.get(name=job_group_name)
+                            Category.objects.get_or_create(
+                                group=group, name=job_category.text
+                            )
+                        # time.sleep(3)
+                        finds_visible(driver, wait, "button[class*=Button_Button]")[
+                            0
+                        ].click()
+                        print(f"button click")
+                        # time.sleep(3)
+                    driver.quit()
+                    print("크롤링을 종료합니다.")
+                    break
+                except:
+                    delay += 0.5
+                    retry += 1
+
+                    if retry == 3:
+                        raise Exception("3회 재시도에도 실패하였습니다.")
+        else:
+            print("데이터가 존재하기 때문에 크롤링을 진행하지 않습니다.")
