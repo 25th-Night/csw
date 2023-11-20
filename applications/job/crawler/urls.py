5da8967f88c850150307c5e5c3480cdce799e125
25th-Night <brown_eyed87@naver.com>
2023-11-20 11:44:30 +0900

    update)urls: 생성한 뷰의 url 등록, 기존 url 수정

--- applications/job/crawler/urls.py
+++ applications/job/crawler/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from crawler.views import (
     RecruitDetailView,
     RecruitView,
+    SkillSearchView,
 )
 
 
 urlpatterns = [
-    path("recruits/", RecruitView.as_view(), name="recruits"),
-    path("recruits/<int:url>", RecruitDetailView.as_view(), name="recruit"),
+    path("recruits", RecruitView.as_view(), name="crawling_recruits"),
+    path("recruits/<int:url>", RecruitDetailView.as_view(), name="crawling_recruit"),
+    path("skill/search", SkillSearchView.as_view(), name="search_skill"),
 ]
