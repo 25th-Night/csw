826e0b61334186ba89f06379f6c7e2e94c91c544
25th-Night <brown_eyed87@naver.com>
2023-11-26 00:56:31 +0900

    update)permissions: login 요청 시 request.path 추가

--- applications/user/common/permissions.py
+++ applications/user/common/permissions.py
@@ -1,13 +1,19 @@
 from django.contrib.auth.mixins import AccessMixin
 from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class LoginRequired(AccessMixin):
     def dispatch(self, request, *args, **kwargs):
+        print(f"request.path:{request.path}")
         if (
             not hasattr(request.user, "is_authenticated")
             or not request.user.is_authenticated
         ):
-            return redirect("login")
+            if request.path == reverse("login"):
+                return redirect("login")
+            else:
+                redirect_url = f"/login/?next={request.path}"
+                return redirect(redirect_url)
 
         return super().dispatch(request, *args, **kwargs)
